0328d08401c453d5cdae1dba417b6f2dac1ed2cf
LX6T <alex@pullen.net.nz>
2022-09-25 20:18:52 +1300

    refactor - made minor changes to the ProfileRepository class and added javadoc comments to its methods

--- src/main/java/nz/ac/auckland/se206/user/ProfileRepository.java
+++ src/main/java/nz/ac/auckland/se206/user/ProfileRepository.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Objects;
 
 public class ProfileRepository {
 
@@ -16,16 +15,33 @@ public class ProfileRepository {
   private final Path repoPathName;
   private HashMap<String, UserProfile> users;
 
+  /**
+   * Constructs an empty ProfileRepository
+   *
+   * @param pathName of repository
+   */
   public ProfileRepository(String pathName) {
     repoPathName = Paths.get(pathName);
     users = new HashMap<>();
     loadProfiles();
   }
 
+  /**
+   * Gets the HashMap containing the user profiles
+   *
+   * @return users HashMap
+   */
   public HashMap<String, UserProfile> getUsers() {
     return users;
   }
 
+  /**
+   * If the username doesn't already exist, the user profile is added to the HashMap and returns
+   * true Otherwise, the method does nothing and returns false
+   *
+   * @param profile to be added to the HashMap
+   * @return false if username is taken, otherwise return true
+   */
   public boolean saveProfile(UserProfile profile) {
     String username = profile.getAccountName();
     if (users.containsKey(username)) {
@@ -36,6 +52,11 @@ public class ProfileRepository {
     }
   }
 
+  /**
+   * Save the HashMap containing all the current user profiles
+   *
+   * @return true
+   */
   public boolean saveProfiles() {
     String usersJson = gson.toJson(users);
     try {
@@ -47,15 +68,16 @@ public class ProfileRepository {
     }
   }
 
+  /**
+   * Load user profiles from repository into the HashMap
+   *
+   * @return true if load is successful, otherwise return false
+   */
   public boolean loadProfiles() {
     try {
       String userJson = Files.readString(repoPathName);
-      if (Objects.equals(userJson, "")) {
-        users = new HashMap<>();
-      } else {
-        Type type = new TypeToken<HashMap<String, UserProfile>>() {}.getType();
-        users = gson.fromJson(userJson, type);
-      }
+      Type type = new TypeToken<HashMap<String, UserProfile>>() {}.getType();
+      users = gson.fromJson(userJson, type);
       return true;
     } catch (Exception e) {
       e.printStackTrace();
@@ -63,14 +85,8 @@ public class ProfileRepository {
     }
   }
 
-  public boolean clearRepository() {
-    try {
-      users = new HashMap<>();
-      Files.writeString(repoPathName, "");
-      return true;
-    } catch (Exception e) {
-      e.printStackTrace();
-      return false;
-    }
+  /** Clears the HashMap */
+  public void clearRepository() {
+    users = new HashMap<>();
   }
 }
