62cf2b743245cfdf351fd088923d370e43215d33
LX6T <alex@pullen.net.nz>
2022-09-25 19:04:23 +1300

    feat - ProfileRepository can now clear and load user profiles from the repository

--- src/main/java/nz/ac/auckland/se206/user/ProfileRepository.java
+++ src/main/java/nz/ac/auckland/se206/user/ProfileRepository.java
@@ -2,21 +2,28 @@ package nz.ac.auckland.se206.user;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class ProfileRepository {
 
   private static final Gson gson = new GsonBuilder().create();
-
   private final Path repoPathName;
   private HashMap<String, UserProfile> users;
 
   public ProfileRepository(String pathName) {
     repoPathName = Paths.get(pathName);
     users = new HashMap<>();
+    loadProfiles();
+  }
+
+  public HashMap<String, UserProfile> getUsers() {
+    return users;
   }
 
   public boolean saveProfile(UserProfile profile) {
@@ -33,6 +40,33 @@ public class ProfileRepository {
     String usersJson = gson.toJson(users);
     try {
       Files.writeString(repoPathName, usersJson);
+      return true;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+  public boolean loadProfiles() {
+    try {
+      String userJson = Files.readString(repoPathName);
+      if (Objects.equals(userJson, "")) {
+        users = new HashMap<>();
+      } else {
+        Type type = new TypeToken<HashMap<String, UserProfile>>() {}.getType();
+        users = gson.fromJson(userJson, type);
+      }
+      return true;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+  public boolean clearRepository() {
+    try {
+      users = new HashMap<>();
+      Files.writeString(repoPathName, "");
       return true;
     } catch (Exception e) {
       e.printStackTrace();
