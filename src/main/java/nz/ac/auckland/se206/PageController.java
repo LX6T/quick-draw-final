c793e00c88bbad1eed0eb5784ee2b996601eef47
Yuqi <yxin202@aucklanduni.ac.nz>
2022-09-26 19:37:21 +1300

    feature - created sign up functionalities which help to register new users

--- src/main/java/nz/ac/auckland/se206/PageController.java
+++ src/main/java/nz/ac/auckland/se206/PageController.java
@@ -2,70 +2,86 @@ package nz.ac.auckland.se206;
 
 import java.io.IOException;
 import java.util.HashMap;
-
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import nz.ac.auckland.se206.user.ProfileRepository;
 import nz.ac.auckland.se206.user.UserProfile;
 
 public class PageController {
-	
-	private ProfileRepository repo;
-	private HashMap<String, UserProfile> users;
 
-	@FXML
-	private Button buttonOnStart;
-	@FXML
-	private Button buttonOnExit;
-	@FXML
-	private Button buttonOnSignIn;
-	
-	
-	
-	public void initialise() {
-		repo = new ProfileRepository("src/main/java/nz/ac/auckland/se206/profiles/repository");
-		repo.loadProfiles();
-		this.users = repo.getUsers();
-	}
+  private ProfileRepository repo =
+      new ProfileRepository("src/main/java/nz/ac/auckland/se206/profiles/user profile repository");
+  private HashMap<String, UserProfile> users = repo.getUsers();
 
-	@FXML
-	private void exitGame() {
-		Platform.exit();
-		System.exit(0);
-	}
+  @FXML private Button buttonOnStart;
+  @FXML private Button buttonOnExit;
+  @FXML private Button buttonOnSignIn;
+  @FXML private Button buttonOnSignUp;
+  @FXML private TextField userName;
 
-	@FXML
-	private void signInAction(ActionEvent event) throws InterruptedException {
-		if(true) {
-			Button button = (Button) event.getSource();
-			Scene sceneButtonIsIn = button.getScene();
+  public void initialise() {
+    repo.loadProfiles();
+  }
 
-			try {
-				// load the canvas scene when press this button
-				sceneButtonIsIn.setRoot(App.loadFxml("profilePage"));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+  @FXML
+  private void exitGame() {
+    Platform.exit();
+    System.exit(0);
+  }
 
-	@FXML
-	private void startNewGame(ActionEvent event) throws InterruptedException {
+  @FXML
+  private void signInAction(ActionEvent event) throws InterruptedException {
+    if (true) {
+      Button button = (Button) event.getSource();
+      Scene sceneButtonIsIn = button.getScene();
 
-		Button button = (Button) event.getSource();
-		Scene sceneButtonIsIn = button.getScene();
+      try {
+        // load the canvas scene when press this button
+        sceneButtonIsIn.setRoot(App.loadFxml("profilePage"));
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+  }
 
-		try {
-			// load the canvas scene when press this button
-			sceneButtonIsIn.setRoot(App.loadFxml("canvas"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+  @FXML
+  private void startNewGame(ActionEvent event) throws InterruptedException {
 
+    Button button = (Button) event.getSource();
+    Scene sceneButtonIsIn = button.getScene();
+
+    try {
+      // load the canvas scene when press this button
+      sceneButtonIsIn.setRoot(App.loadFxml("canvas"));
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  @FXML
+  private void onSignUp() {
+    if (userName.getText().isBlank()) {
+      Alert alert = new Alert(AlertType.ERROR);
+      alert.setTitle("Empty username");
+      alert.setHeaderText("Please insert a valid username and try again");
+      alert.showAndWait();
+    } else if (users.containsKey(userName.getText())) {
+      Alert alert = new Alert(AlertType.ERROR);
+      alert.setTitle("User name already exists");
+      alert.setHeaderText("Please click on Sign-In to start the game");
+      alert.showAndWait();
+    } else {
+      UserProfile newUser = new UserProfile(userName.getText());
+      repo.saveProfile(newUser);
+      repo.saveProfiles();
+    }
+  }
 }
