8fbd5d6b61287cb1275946a3fc2085027c1434e4
LX6T <alex@pullen.net.nz>
2022-10-09 16:53:58 +1300

    refactor - Blanket refactoring for better readability

--- src/main/java/nz/ac/auckland/se206/PageController.java
+++ src/main/java/nz/ac/auckland/se206/PageController.java
@@ -1,9 +1,6 @@
 package nz.ac.auckland.se206;
 
 import java.io.IOException;
-
-import com.jfoenix.controls.JFXButton;
-
 import javafx.animation.FadeTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -19,130 +16,109 @@ import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 import nz.ac.auckland.se206.user.ProfileRepository;
 import nz.ac.auckland.se206.user.UserProfile;
+import nz.ac.auckland.se206.util.TransitionUtils;
 
 public class PageController {
 
-	@FXML
-	private JFXButton buttonOnStart;
-	@FXML
-	private JFXButton buttonOnExit;
-	@FXML
-	private JFXButton buttonOnSignIn;
-	@FXML
-	private JFXButton buttonOnSignUp;
-	@FXML
-	private TextField userName;
-	@FXML
-	private AnchorPane masterPane;
+  @FXML private TextField userName;
+  @FXML private AnchorPane masterPane;
 
-	@FXML
-	private void exitGame() {
-		Platform.exit();
-		System.exit(0);
-	}
+  @FXML
+  private void exitGame() {
+    Platform.exit();
+    System.exit(0);
+  }
 
-	@FXML
-	private void signInAction(ActionEvent event) throws InterruptedException {
-		if (userName.getText().isBlank()) {
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setTitle("Empty username");
-			alert.setHeaderText("Please insert a valid username and try again");
-			alert.showAndWait();
-			// show and wait a bit to let user see
-		} else if (ProfileRepository.containsKey(userName.getText())) {
-			fadeOutTwo(event);
+  @FXML
+  private void signInAction(ActionEvent event) {
+    if (userName.getText().isBlank()) {
+      Alert alert = new Alert(AlertType.ERROR);
+      alert.setTitle("Empty username");
+      alert.setHeaderText("Please insert a valid username and try again");
+      alert.showAndWait();
+      // show and wait a bit to let user see
+    } else if (ProfileRepository.containsKey(userName.getText())) {
+      fadeOutTwo(event);
 
-		} else {
-			Alert alert = new Alert(AlertType.ERROR);
-			// throwing alerts if needed
-			alert.setTitle("User name does not exist");
-			alert.setHeaderText("Please click on Sign-Up to create your account");
-			alert.showAndWait();
-		}
-	}
+    } else {
+      Alert alert = new Alert(AlertType.ERROR);
+      // throwing alerts if needed
+      alert.setTitle("User name does not exist");
+      alert.setHeaderText("Please click on Sign-Up to create your account");
+      alert.showAndWait();
+    }
+  }
 
-	@FXML
-	private void startNewGame(ActionEvent event) throws InterruptedException {
-		fadeOut(event);
+  @FXML
+  private void startNewGame(ActionEvent event) {
+    fadeOut(event);
+  }
 
-	}
+  private void fadeOut(ActionEvent event) {
+    FadeTransition ft = TransitionUtils.getFadeTransition(masterPane);
+    ft.setOnFinished((ActionEvent eventTwo) -> loadCanvasScene(event));
+    ft.play();
+  }
 
-	private void fadeOut(ActionEvent event) {
-		// TODO Auto-generated method stub
-		FadeTransition ft = new FadeTransition();
-		ft.setDuration(Duration.millis(500));
-		ft.setNode(masterPane);
-		ft.setFromValue(1);
-		ft.setToValue(0.2);
-		ft.setOnFinished((ActionEvent eventTwo) -> {
-			loadNextScene(event);
-		});
-		ft.play();
+  private void fadeOutTwo(ActionEvent event) {
+    FadeTransition ft = new FadeTransition();
+    ft.setDuration(Duration.millis(500));
+    ft.setNode(masterPane);
+    ft.setFromValue(1);
+    ft.setToValue(0.2);
+    ft.setOnFinished(
+        (ActionEvent eventTwo) -> {
+          UserProfile currentUser = ProfileRepository.get(userName.getText());
+          ProfileRepository.setCurrentUser(currentUser);
+          Button button = (Button) event.getSource();
+          Scene sceneButtonIsIn = button.getScene();
+          FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/profilePage.fxml"));
+          Parent root = null;
+          try {
+            root = loader.load();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+          StatsController statsController = loader.getController();
+          // get the controller from the stats controller
+          statsController.setStats(currentUser);
+          sceneButtonIsIn.setRoot(root);
+          // james will see what the root is
+        });
+    ft.play();
+  }
 
-	}
+  private void loadCanvasScene(ActionEvent event) {
+    Button button = (Button) event.getSource();
+    Scene sceneButtonIsIn = button.getScene();
 
-	private void fadeOutTwo(ActionEvent event) {
-		// TODO Auto-generated method stub
-		FadeTransition ft = new FadeTransition();
-		ft.setDuration(Duration.millis(500));
-		ft.setNode(masterPane);
-		ft.setFromValue(1);
-		ft.setToValue(0.2);
-		ft.setOnFinished((ActionEvent eventTwo) -> {
-			UserProfile currentUser = ProfileRepository.get(userName.getText());
-			ProfileRepository.setCurrentUser(currentUser);
-			Button button = (Button) event.getSource();
-			Scene sceneButtonIsIn = button.getScene();
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/profilePage.fxml"));
-			Parent root = null;
-			try {
-				root = loader.load();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			StatsController statsController = loader.getController();
-			// get the controller from the stats controller
-			statsController.setStats(currentUser);
-			sceneButtonIsIn.setRoot(root);
-			// james will see what the root is
-		});
-		ft.play();
+    try {
+      // load the canvas scene when press this button
+      sceneButtonIsIn.setRoot(App.loadFxml("canvas"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-	}
-
-	private void loadNextScene(ActionEvent event) {
-		Button button = (Button) event.getSource();
-		Scene sceneButtonIsIn = button.getScene();
-
-		try {
-			// load the canvas scene when press this button
-			sceneButtonIsIn.setRoot(App.loadFxml("canvas"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	@FXML
-	private void onSignUp() {
-		if (userName.getText().isBlank()) {
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setTitle("Empty username");
-			alert.setHeaderText("Please insert a valid username and try again");
-			alert.showAndWait();
-			// ig the user is blank then pop out alter message
-		} else if (ProfileRepository.containsKey(userName.getText())) {
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setTitle("User name already exists");
-			alert.setHeaderText("Please click on Sign-In to start the game");
-			alert.showAndWait();
-			// if the user already exist also throw the alter message
-		} else {
-			// if the user does not exist then sign up
-			UserProfile newUser = new UserProfile(userName.getText());
-			ProfileRepository.saveProfile(newUser);
-			ProfileRepository.updateProfiles();
-		}
-	}
+  @FXML
+  private void onSignUp() {
+    if (userName.getText().isBlank()) {
+      Alert alert = new Alert(AlertType.ERROR);
+      alert.setTitle("Empty username");
+      alert.setHeaderText("Please insert a valid username and try again");
+      alert.showAndWait();
+      // ig the user is blank then pop out alter message
+    } else if (ProfileRepository.containsKey(userName.getText())) {
+      Alert alert = new Alert(AlertType.ERROR);
+      alert.setTitle("User name already exists");
+      alert.setHeaderText("Please click on Sign-In to start the game");
+      alert.showAndWait();
+      // if the user already exist also throw the alter message
+    } else {
+      // if the user does not exist then sign up
+      UserProfile newUser = new UserProfile(userName.getText());
+      ProfileRepository.saveProfile(newUser);
+      ProfileRepository.updateProfiles();
+    }
+  }
 }
